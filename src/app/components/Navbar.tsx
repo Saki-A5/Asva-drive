2e46c5085a371f9a37ec2e59b09cc0e5dbee78c7
Solomon Tolulope <kaysaki63@gmail.com>
2025-08-13 14:46:36 +0100

    replacing existing work with new work

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,53 @@
+"use client"
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const Navbar  = () => {
+    const { setTheme } = useTheme()
 
     return (
-    <div className=" flex h-20">
+    <div className=" flex h-20 pt-2">
         <Link href="/">
-        <img src="/asva logo.jpg" alt="logo" width={60} height={60}/>
+        <h2 className="font-bold ml-4">ASVA</h2>
         </Link>
 
-        <div className="gap-4 flex">
+        <div className="gap-2 flex ml-auto mr-4">
             <Link href="#" >
-                <Button>Sign up</Button>
+                <Button className="w">Sign up</Button>
             </Link>
         
             <Link href="#">
                 <Button>Log in</Button>
             </Link>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="icon">
+                        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                        <span className="sr-only">Toggle theme</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                        Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                        Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                        System
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     </div>
     )
