ef26bbae106f3ca4d6902fc4cde76593706142e9
Solomon Tolulope <kaysaki63@gmail.com>
2025-11-19 15:14:53 +0100

    replacing existing work with new work

--- src/app/components/Upload.tsx
+++ src/app/components/Upload.tsx
@@ -7,7 +7,7 @@ const Upload = () => {
 
     return (
         <>
-        <Button variant="outline" className="bg-black text-white">
+        <Button variant="outline" className="bg-[#001f3f] text-white hover:bg-[#001f3f] hover:text-white transition-none">
             <span className="flex">
                 <UploadIcon />
                 <span className="pl-2">Upload</span>
