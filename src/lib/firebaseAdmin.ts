3713eb1c5f5b9eb6723816797a6c94d084e38587
Solomon Tolulope <kaysaki63@gmail.com>
2025-12-28 01:15:34 +0100

    replacing existing work with new work

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -2,7 +2,7 @@ import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
   const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT as string
+    process.env.FIREBASE_SERVICE_ACCOUNT!
   );
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
