bf63164ff56fb5fe037f39f4f906ec21457b9907
Solomon Tolulope <kaysaki63@gmail.com>
2025-12-29 06:49:42 +0100

    replacing existing work with new work

--- src/app/components/Floating.tsx
+++ src/app/components/Floating.tsx
@@ -4,12 +4,15 @@ import Upload from "./Upload"
 import useCurrentUser from "@/hooks/useCurrentUser"
 
 const Floating = () => {
+    const handleCreateFolder = () => {
+    console.log('Create folder clicked');
+  }
     const {user} = useCurrentUser();
 
     return (
         <div className="fixed bottom-4 right-4 flex flex-col gap-4 z-50 sm:hidden">
             {user?.role === 'admin' && <Upload />}
-            <Create />
+            <Create onCreateFolderClick={handleCreateFolder} />
         </div>
     )
 }
