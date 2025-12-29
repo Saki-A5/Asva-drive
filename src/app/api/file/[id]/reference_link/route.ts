0ae73583c01951e217238432e7d3c54bb6416c79
Okoronkwo Daniel <danielchukwuemela751@gmail.com>
2025-12-29 12:50:26 +0100

    refactored the file referencing model

--- src/app/api/file/[id]/reference_link/route.ts
+++ src/app/api/file/[id]/reference_link/route.ts
@@ -5,6 +5,7 @@ import User from "@/models/users";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { Types } from "mongoose";
+import FileItemModel from "@/models/fileItem";
 
 export const runtime = 'nodejs';
 
@@ -14,7 +15,7 @@ export const POST = async (req: Request, { params }: any) => {
 
         const { parentFolderId, tags } = await req.json();
 
-        const { id: fileId } = await params;
+        const { id: fileItemId } = await params;
 
         const cookieStore = await cookies();
         const token = cookieStore.get("token")?.value;
@@ -30,29 +31,23 @@ export const POST = async (req: Request, { params }: any) => {
         const user = await User.findOne({ email });
         if (!user) return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-        const file = await FileModel.findById(fileId);
-        if (!file) return NextResponse.json({ error: "File Not Found" }, { status: 404 });
-        if (file.isFolder) return NextResponse.json({ error: "You can't reference Link Folders" }, { status: 400 });
+        const fileItem = await FileItemModel.findById(fileItemId);
+        if (!fileItem) return NextResponse.json({ error: "File Not Found" }, { status: 404 });
+        if (fileItem.isFolder) return NextResponse.json({ error: "You can't reference Link Folders" }, { status: 400 });
 
-        const folder = await FileModel.findOne({ ownerId: user._id, _id: new Types.ObjectId(parentFolderId) });
+        const folder = await FileItemModel.findOne({ ownerId: user._id, _id: new Types.ObjectId(parentFolderId) });
         if (!folder) return NextResponse.json({ error: "Folder not found" }, { status: 404 });
 
-        const createdFile = await FileModel.create({
-            filename: file.filename,
-            cloudinaryUrl: file.cloudinaryUrl,
+        const createdFileItem = await FileItemModel.create({
+            filename: fileItem.filename,
             ownerId: user._id,
-            extractedText: file.extractedText,
-            resourceType: file.resourceType,
-            mimeType: file.mimeType,
-            sizeByte: file.sizeBytes,
-            tags: file.tags.concat(tags),
-            reference: {
-                isReference: true,
-                referencedFile: file._id
-            },
+            parentFolderId: new Types.ObjectId(parentFolderId),
+            ownerType: "User", 
+            isReference: true, 
+            file: fileItem.file
         });
 
-        return NextResponse.json({ message: "Successfully referenced linked file", file: createdFile });
+        return NextResponse.json({ message: "Successfully referenced linked file", file: createdFileItem });
     }
     catch (e: any) {
         return NextResponse.json({ error: e.message || "An Error Occurred" }, { status: 500 });
