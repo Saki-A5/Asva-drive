d1c6b14b0b0e61be7288aa890f9dcd6bfe1899ec
Solomon Tolulope <kaysaki63@gmail.com>
2026-01-07 12:38:52 +0100

    replacing existing work with new work

--- src/app/components/PdfViewer.tsx
+++ src/app/components/PdfViewer.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import "react-pdf/dist/esm/Page/TextLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
 import { useState } from "react";
 
 pdfjs.GlobalWorkerOptions.workerSrc =
